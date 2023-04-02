86be465f1d8992425f5156543099d51f247156d6
aripsubekti <rifaa.subekti@students.amikom.ac.id>
2023-04-02 20:57:08 +0700

    menambahkan source code aplikasi taxi

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Rif'aa Surososastro Subekti
+// 22.11.5060
+
+namespace AplikasiTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,46 @@
+﻿// Rif'aa Surososastro Subekti
+// 22.11.5060
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikasiTaxi
+{
+    public class Taxi
+    {
+        // properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        // method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name          : {0}", DriverName);
+
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty              : Yes", OnDuty);
+            }
+            else
+            {
+                Console.WriteLine("On Duty              : No", OnDuty);
+            }
+            
+            Console.WriteLine("Number of Passenger  : {0}\n", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
